bc55f9c1f635daa6a099b4991af4ad8221c1a8df
jinho17 <angry123456@nate.com>
2021-03-27 02:29:40 +0900

    add response body

--- response/Response.cpp
+++ response/Response.cpp
@@ -1,58 +1,25 @@
 #include "Response.hpp"
 
-Response::Response(std::string status_code, std::string status_msg, ConfigParse::t_server *conf_server, std::string content_path, std::map<std::string, std::string> req_header, std::string content_length)
+Response::Response(std::string body):
+	body(body)
 {
-	header["http"] = "1.1";
-	header.insert(make_pair("status_code", status_code));
-	header.insert(make_pair("status_msg", status_msg));
-
-	setDate();
-	setServer(conf_server);
-	setContentType(content_path);
-	setContentLength(req_header, content_length);
 }
 
-Response::~Response() {};
-
-void Response::setDate()
+Response::~Response()
 {
-	struct timeval curr;
-	struct tm time;
-
-	gettimeofday(&curr, NULL);
-	strptime(std::to_string(curr.tv_sec).c_str(), "%s", &time);
-	header.insert(make_pair("Date", ft_makeGMT(time.tm_zone, curr.tv_sec)));
 }
 
-void Response::setServer(ConfigParse::t_server *conf_server)
+void Response::makeRes(int status_code, ConfigParse::t_server *conf_server, std::string content_type, std::map<std::string, std::string> req_header, std::string content_length)
 {
-	header.insert(make_pair("Server", conf_server->name));
-}
+	ResponseHeader res(status_code, conf_server, content_type, req_header, content_length);
 
-void Response::setContentType(std::string content_path)
-{
-	std::string type = content_path.substr(content_path.find('.') + 1);
-
-	if (type == "html" || type == "htm")
-		header["Content-Type"] = "text/html";
-	else if (type == "css")
-		header["Content-Type"] = "text/css";
-	else if (type == "js")
-		header["Content-Type"] = "text/javascript";
-	else if (type == "jpeg" || type == "jpg")
-		header["Content-Type"] = "image/jpeg";
-	else
-		header["Content-Type"] = "text/plain";
-	
-	//https://developer.mozilla.org/ko/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
-	//else if (type == "png")
-	//	header["Content-Type"] = "png";
-	//else if (type == "bmp")
-	//	header["Content-Type"] = "bmp";
-}
-
-void Response::setContentLength(std::map<std::string, std::string> req_header, std::string content_length)
-{
-	if (req_header.find("Transfer-Encoding")->second != "chunked")
-		header.insert(make_pair("Content-Length", content_length));
+	res_str = "HTTP/" + res.header["http"] + " " + res.header["status_code"] + " " + res.header["status_msg"] + "\r\n"
+				+ "Date: " + res.header["Date"] + "\r\n"
+				+ "Server: " + res.header["Server"] + "\r\n"
+				+ "Content-Type: " + res.header["Content-Type"] + "\r\n";
+	if (res.header.find("Content-Length") != res.header.end())
+		res_str += "Content-Length: " + res.header["Content-Length"] + "\r\n";
+	res_str += "\r\n";
+	if (res.header["status_code"] != "201" && res.header["status_code"] != "204")
+		res_str += body;
 }
