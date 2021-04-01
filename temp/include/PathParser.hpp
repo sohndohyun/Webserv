97017a171f3b57bc783e56315b7840e4040c89c7
Jinho Kim <jinkim@c1r8s3.42seoul.kr>
2021-04-01 17:21:15 +0900

    update

---
+++ temp/include/PathParser.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+class PathParser
+{
+public:
+	std::string path;
+	std::map<std::string, std::string>* query;
+	std::string fragment;
+public:
+	PathParser();
+	PathParser(const std::string& s);
+	virtual ~PathParser();
+private:
+	std::map<std::string, std::string>* splitIntoMap(std::string s, char sp1, char sp2);
+};
