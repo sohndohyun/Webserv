73ef6433c7b2f6d1005b27471e928bd7f33ec150
Jinho Kim <jinkim@c1r2s1.42seoul.kr>
2021-04-30 15:38:45 +0900

    modify plugin thread num

--- srcs_bonus/main_bonus.cpp
+++ srcs_bonus/main_bonus.cpp
@@ -14,7 +14,12 @@ int main(int argc, char *argv[])
 
 		ConfigParse cp(plugin.plugin.config);
 		WebServer webServer(cp, plugin.plugin);
-		webServer.run("0.0.0.0", cp.port);
+		//std::cout << "config thread : " << cp.thread_num << std::endl;
+		//std::cout << "plugin thread : " << plugin.plugin.thread << std::endl;
+		if (plugin.plugin.thread != 0)
+			webServer.run("0.0.0.0", cp.port, plugin.plugin.thread);
+		else
+			webServer.run("0.0.0.0", cp.port, cp.thread_num);
 	} catch(const std::exception& e) {
 		std::cerr << e.what() << '\n';
 	}
